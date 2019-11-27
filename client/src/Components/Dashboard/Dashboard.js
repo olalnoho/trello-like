fb467a7bf083e45867a29fe03dfd63520c05151c
unknown <alexander.norberghoff@gmail.com>
2019-11-27 14:55:30 +0100

    dashboard

---
+++ client/src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Dashboard = () => {
+   return (
+      <div className="container dashboard">
+         <div className="dashboard__add">
+            <strong>Add project</strong>
+            <div className="pointer">
+               <span className="add-project"></span>
+            </div>
+         </div>
+         <div className="dashboard__projects">
+            <div className="project">
+               <h3>Project 1</h3>
+            </div>
+            <div className="project">
+               <h3>Project 2</h3>
+            </div>
+            <div className="project">
+               <h3>Project 3</h3>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default Dashboard
