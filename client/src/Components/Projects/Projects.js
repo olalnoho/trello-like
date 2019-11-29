9f7f7ae250199e5dcf9e797b4cef898f4e16d85d
unknown <alexander.norberghoff@gmail.com>
2019-11-29 20:53:10 +0100

    dnd saves position now

--- client/src/Components/Projects/Projects.js
+++ client/src/Components/Projects/Projects.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState, useCallback } from 'react'
+import { DragDropContext } from 'react-beautiful-dnd'
 import axios from 'axios'
+
+
 import Modal from '../UI/Modal'
 import AddForm from '../Dashboard/AddForm'
 import List from './List'
+
 const Projects = props => {
    const id = props.match.params.id
 
@@ -15,6 +19,18 @@ const Projects = props => {
             setLists(data)
          })
    }, [id])
+
+   const onDragEnd = result => {
+      const { source, destination, draggableId } = result
+
+      if(!destination) return
+
+      const fromContainer = lists.find(list => list._id === source.droppableId)
+      const toContainer = lists.find(list => list._id === destination.droppableId)
+
+      const [elem] = fromContainer.tasks.splice(source.index, 1)
+      toContainer.tasks.splice(destination.index, 0, elem)
+   }
 
    const closeModal = useCallback(e => {
       setShowModal(false)
@@ -34,18 +50,19 @@ const Projects = props => {
          >
             <AddForm url={`/api/lists/${id}`} setProjects={setLists} closeModal={closeModal} />
          </Modal>
-
-         <div className="container projects">
-            <div className="dashboard__add">
-               <h3 className="heading-3 light">Add List</h3>
-               <div onClick={e => setShowModal(true)} className="pointer">
-                  <span className="add-project"></span>
+         <DragDropContext onDragEnd={onDragEnd}>
+            <div className="container projects">
+               <div className="dashboard__add">
+                  <h3 className="heading-3 light">Add List</h3>
+                  <div onClick={e => setShowModal(true)} className="pointer">
+                     <span className="add-project"></span>
+                  </div>
+               </div>
+               <div className="projects__lists">
+                  {lists.map(list => <List tasks={list.tasks} projectId={id} key={list._id} list={list} />)}
                </div>
             </div>
-            <div className="projects__lists">
-               {lists.map(list => <List tasks={list.tasks} projectId={id} key={list._id} list={list} />)}
-            </div>
-         </div>
+         </DragDropContext>
       </>
    )
 }
--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/trello', {
+mongoose.connect(process.env.MONGO_URI, {
    useNewUrlParser: true,
    useUnifiedTopology: true
 }).then(() => console.log('Connected to mongoose'))
